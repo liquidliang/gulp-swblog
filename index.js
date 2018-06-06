44d00775a09ad9adb560949fe6664c83cfe66351
liquidliang <ljquan@qq.com>
2018-06-06 15:33:56 +0800

    fix sw url bug

--- index.js
+++ index.js
@@ -5,31 +5,50 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const copy = require('copy');
 const indexerPlugin = require('gulp-markdown-index');
+const packJSON = require('./package.json');
 
 
 module.exports = function (directoryName, options) {
 
-  copy(__dirname + '/source/lib/**/*.*'.replace(/\//g, path.sep), path.resolve(directoryName, './lib'), function (err, file) {
-    if (err) throw err;
-  });
+  // console.log(packJSON.version);
+  let needInit = true;
+  let versionPath = path.resolve(directoryName, './version');
+  try{
+    let version = fs.readFileSync(versionPath, 'utf8');
+    if (version == packJSON.version){
+      needInit = false;
+    }
+  }catch(e){}
+  if(needInit){
+    console.log('needInit')
+    copy(__dirname + '/source/lib/**/*.*'.replace(/\//g, path.sep), path.resolve(directoryName, './lib'), function (err, file) {
+      if (err) throw err;
+    });
 
-  copy(__dirname + '/source/dist/*.min.js'.replace(/\//g, path.sep), path.resolve(directoryName, './dist'), function (err, file) {
-    if (err) throw err;
-  });
+    copy(__dirname + '/source/dist/*.min.js'.replace(/\//g, path.sep), path.resolve(directoryName, './dist'), function (err, file) {
+      if (err) throw err;
+    });
 
-  copy(__dirname + '/source/*.html'.replace(/\//g, path.sep), path.resolve(directoryName, './'), function (err, file) {
-    if (err) throw err;
-  });
+    copy(__dirname + '/source/*.html'.replace(/\//g, path.sep), path.resolve(directoryName, './'), function (err, file) {
+      if (err) throw err;
+    });
 
-  copy(__dirname + './*.ico'.replace(/\//g, path.sep), path.resolve(directoryName, './'), function (err, file) {
-    if (err) throw err;
-  });
+    copy(__dirname + './*.ico'.replace(/\//g, path.sep), path.resolve(directoryName, './'), function (err, file) {
+      if (err) throw err;
+    });
 
-  mkdirp(path.resolve(directoryName, './json/'), function (err) {
-    if (err) console.error(err);
-  });
+    mkdirp(path.resolve(directoryName, './json/'), function (err) {
+      if (err) console.error(err);
+    });
+    fs.writeFileSync(versionPath, packJSON.version, {
+      encoding: 'utf8'
+    });
+  }
 
-  fs.writeFileSync(path.resolve(directoryName, './json/config.json'), JSON.stringify(options, null, 2));
+
+  fs.writeFileSync(path.resolve(directoryName, './json/config.json'), JSON.stringify(options, null, 2), {
+    encoding: 'utf8'
+  });
 
 
   return indexerPlugin(path.resolve(directoryName, './json/article.json'), options.staticDir);
